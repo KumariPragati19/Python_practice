e472f9a8a6d9e5d9329cfb256eac206b7dcb2eb7
KumariPragati19 <122395348+KumariPragati19@users.noreply.github.com>
2024-01-26 17:32:45 +0530

    loop in python
    for , and while loop in python

---
+++ 08_for_loop.py
@@ -0,0 +1,11 @@
+fruits=['Banana','Watermelon','Apple','Mango','Orange','Grapes']
+
+for items in fruits:
+    print(items)
+
+#range function in python range(0,n) it will iterate from 0 to n-1.
+for i in range(1,8):
+    print(i)
+
+for i in range(1,8,2):
+    print(i)
---
+++ 08_while_loop.py
@@ -0,0 +1,40 @@
+# i=0
+# while i==0:
+#     print("yes")
+# # above code execute infinite times
+
+#--------------------------------------------------------------
+
+i=0
+while i<10:
+    print("yes"+str(i))
+    i=i+1
+print("Done")
+
+#-------------------------------------------------------------------
+
+# print number 1 to 10
+i=1
+while i<51:
+    print(i)
+    i=i+1
+ 
+# or,
+i=0
+while i<50:
+    i=i+1
+    print(i)
+
+#-------------------------------------------------------
+
+i=0
+while i<5:
+    print('Harry')
+    i=i+1
+#---------------------------------------------------------
+    
+fruits=['Banana','Watermelon','Apple','Mango','Orange','Grapes']
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i=i+1
